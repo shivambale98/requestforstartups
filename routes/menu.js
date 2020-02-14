583d019effd4e67b08c9d0813e5532a07cd58a1a
RMartires <rohitmartires14@gmail.com>
2020-02-14 10:34:46 +0530

    separating the node part

--- routes/menu.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const menucontoler = require('../controllers/menu');
-
-router.get('/getmenu/:token', menucontoler.postmenu);
-
-module.exports = router;
